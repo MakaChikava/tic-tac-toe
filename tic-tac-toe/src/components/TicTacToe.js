fba2729bf31af009242d5e7e79f01cfa870c7d18
makachikava <chikavamakanaka@gmail.com>
2023-08-21 20:00:32 -0700

    setup git ignore and necessary files needed

---
+++ tic-tac-toe/src/components/TicTacToe.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 
+
+const TicTacToe = () => {
+    return (
+        <div>
+            <h1>TicTacToe</h1>
+        </div>
+    );
+}
+
+export default TicTacToe;
